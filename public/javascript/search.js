44cab6ec794d17cec528772b481ae30ba73f7e8e
coderrahul-cyber <rahulsmant299@gmail.com>
2024-05-04 10:11:46 +0530

    updated js

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,38 +1,55 @@
-const sbtn =document.querySelector(".search");
+const sbtn = document.querySelector(".search");
 const left = document.querySelector('.left');
 const icon = document.querySelectorAll('.icon span');
 const parentic = document.querySelectorAll('.icon');
-const last = document.querySelector(".last")
+const last = document.querySelector(".last");
 const ser = document.querySelector(".seee");
-
-
 const insta = document.querySelector(".logo img");
 
-function changesAfterClick(){
-    console.log("clicked")
-    insta.src ="../public/img/default/download.png";
-    insta.style.width = "30px";
-    icon.forEach((icon)=>{
-        icon.style.display = "none";
-    })
-    insta.style.marginBottom = "20px"
-    left.style.flexDirection ="row"
-    last.style.display = "none";
-    left.style.width = "5%";
-    left.style.border = "none";
+let isChanged = false;
 
-    ser.style.display= "block";
-    icon.forEach((icon)=>{
-        icon.style.display = "none";
-    })
-    parentic.forEach((icon)=>{
-        icon.style.width = "40px"
-    })
-   
+function changesAfterClick() {
+    if (!isChanged) {
+        console.log("clicked");
+        insta.src = "../public/img/default/download.png";
+        insta.style.width = "30px";
+        icon.forEach((icon) => {
+            icon.style.display = "none";
+        });
+        insta.style.marginBottom = "20px";
+        left.style.flexDirection = "row";
+        last.style.display = "none";
+        left.style.width = "5%";
+        left.style.border = "none";
 
+        ser.style.display = "block";
+        parentic.forEach((icon) => {
+            icon.style.width = "40px";
+        });
+        isChanged = true;
+    } else {
+        // Restore default styles
+        insta.src = "../public/img/default/logo.png"; 
+        icon.forEach((icon) => {
+            icon.style.display = "block";
+        });
+        insta.style.width = "";
+        insta.style.marginBottom = ""; 
+        left.style.flexDirection = ""; 
+        last.style.display = ""; 
+        left.style.width = ""; 
+        left.style.border = "";
+
+        ser.style.display = "none"; 
+        parentic.forEach((icon) => {
+            icon.style.width = ""; 
+        });
+        isChanged = false;
+    }
 }
 
-sbtn.addEventListener("click" , ()=>{
-   changesAfterClick();
-})
+sbtn.addEventListener("click", () => {
+    changesAfterClick();
+});
+
 
